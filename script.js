1aa41501da79141d25558e2fb5902988bb1f7b48
Sasha <kupchenko555@gmail.com>
2023-10-20 10:16:21 +0300

    Photo, Blog

--- script.js
+++ script.js
@@ -18,3 +18,43 @@ function toggleImage() {
 
   isImage1Visible = !isImage1Visible;
 }
+
+
+const gallery = document.querySelector('.gallery');
+const blocks = document.querySelectorAll('.block');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let currentBlockIndex = 0;
+
+// Функція для анімації з'явлення блоку
+function animateInBlock(index) {
+  blocks[index].style.transform = 'translateX(0)';
+  setTimeout(() => {
+    blocks[index].style.opacity = 1;
+    blocks[index].classList.remove('hidden');
+  }, 10);
+}
+
+// Запустити анімацію для першого блоку при завантаженні сторінки
+animateInBlock(currentBlockIndex);
+
+nextButton.addEventListener('click', () => {
+  blocks[currentBlockIndex].style.transform = 'translateX(-100%)';
+  blocks[currentBlockIndex].style.opacity = 0;
+  setTimeout(() => {
+    blocks[currentBlockIndex].classList.add('hidden');
+    currentBlockIndex = (currentBlockIndex + 1) % blocks.length;
+    animateInBlock(currentBlockIndex);
+  }, 500);
+});
+
+prevButton.addEventListener('click', () => {
+  blocks[currentBlockIndex].style.transform = 'translateX(100%)';
+  blocks[currentBlockIndex].style.opacity = 0;
+  setTimeout(() => {
+    blocks[currentBlockIndex].classList.add('hidden');
+    currentBlockIndex = (currentBlockIndex - 1 + blocks.length) % blocks.length;
+    animateInBlock(currentBlockIndex);
+  }, 500);
+});
